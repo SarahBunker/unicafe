8afd91a3db5fb947133de3484f9e5e13edfddc85
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-14 12:37:56 -0700

    addding button component to add feedback and statistics to display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { useState } from 'react'
 
+const Title = ({title}) => {
+  return <h1>{title}</h1>;
+}
+
+const Button = ({clickFunction, text}) => {
+  return <button onClick={clickFunction}>{text}</button>
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <Title title={"give feedback"} />
+      <div>
+        <Button clickFunction={() => {setGood(good+1)}} text={"good"} />
+        <Button clickFunction={() => {setNeutral(neutral+1)}} text={"neutral"} />
+        <Button clickFunction={() => {setBad(bad+1)}} text={"bad"} />
+      </div>
+      <Title title={"statistics"} />
+      <div>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+      </div>
+    </>
   )
 }
 
